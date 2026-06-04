e166b282d9f64bade08601ecef81fedb4b0c65eb
Saurabhsingh708 <saurabhsingh237232@gmail.com>
2026-06-04 21:13:52 +0530

    Day4 assignment is uploaded

---
+++ Day4/Ques16.c
@@ -0,0 +1,30 @@
+// Write a program to Print Armstrong numbers in a range.
+#include <stdio.h>
+#include <math.h>
+int main(){
+int start, end, num, originalNum, remainder, digits;
+int result;
+printf("Enter starting number: ");
+scanf("%d", &start);
+printf("Enter ending number: ");
+scanf("%d", &end);
+printf("Armstrong numbers between %d and %d are:\n", start, end);
+for (num = start; num <= end; num++){
+originalNum = num;
+digits = 0;
+result = 0;
+while (originalNum != 0){
+originalNum /= 10;
+digits++;
+}
+originalNum = num;
+while (originalNum != 0){
+remainder = originalNum % 10;
+result += pow(remainder, digits);
+originalNum /= 10;
+}
+if (result == num)
+printf("%d ", num);
+}
+return 0;
+}
