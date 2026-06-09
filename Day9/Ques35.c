da13d5e8c97a9a1f10696ed0435460ab64a20233
Saurabhsingh708 <saurabhsingh237232@gmail.com>
2026-06-09 20:53:40 +0530

    Day9 assignment is uploaded

---
+++ Day9/Ques35.c
@@ -0,0 +1,16 @@
+// Write a program to Print repeated character pattern
+#include <stdio.h>
+int main(){
+int i, j, n;
+char ch = 'A';
+printf("Enter number of rows: ");
+scanf("%d", &n);
+for(i = 1; i <= n; i++) {
+for(j = 1; j <= i; j++) {
+printf("%c ", ch);
+}
+printf("\n");
+ch++;   
+}
+return 0;
+}
