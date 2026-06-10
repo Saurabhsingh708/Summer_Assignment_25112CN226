cbdd4576bc6e199a81278199e89630fd37dc088d
Saurabhsingh708 <saurabhsingh237232@gmail.com>
2026-06-10 22:19:51 +0530

    Day10 assignment is uploaded

---
+++ Day10/Ques40.c
@@ -0,0 +1,25 @@
+// Write a program to Print character pyramid
+#include <stdio.h>
+int main(){
+int i, j, rows;
+char ch;
+printf("Enter number of rows: ");
+scanf("%d", &rows);
+for(i = 1; i <= rows; i++) {
+for(j = 1; j <= rows - i; j++) {
+printf(" ");
+}
+ch = 'A';
+for(j = 1; j <= i; j++) {
+printf("%c", ch);
+ch++;
+}
+ch -= 2;
+for(j = 1; j < i; j++) {
+printf("%c", ch);
+ch--;
+}
+printf("\n");
+}
+return 0;
+}
