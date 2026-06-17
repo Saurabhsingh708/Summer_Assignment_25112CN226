92d5a520fc153eefc0ccc4e9aede9b948b7c1bb6
Saurabhsingh708 <saurabhsingh237232@gmail.com>
2026-06-17 21:04:03 +0530

    Day17 assignment is uploaded

---
+++ Day17/Ques67.c
@@ -0,0 +1,35 @@
+// Write a program to Intersection of arrays. 
+#include <stdio.h>
+int main(){
+int a[50], b[50], inter[50];
+int n1, n2, i, j, k = 0, found;
+printf("Enter size of first array: ");
+scanf("%d", &n1);
+printf("Enter elements of first array:\n");
+for(i = 0; i < n1; i++) {
+scanf("%d", &a[i]);
+}
+printf("Enter size of second array: ");
+scanf("%d", &n2);
+printf("Enter elements of second array:\n");
+for(i = 0; i < n2; i++) {
+scanf("%d", &b[i]);
+}
+for(i = 0; i < n1; i++) {
+for(j = 0; j < n2; j++) {
+if(a[i] == b[j]) {
+found = 0;
+for(int x = 0; x < k; x++) {
+if(inter[x] == a[i]) {
+found = 1;
+break;
+}}
+if(!found) {
+inter[k++] = a[i];
+}}}}
+printf("Intersection of arrays: ");
+for(i = 0; i < k; i++) {
+printf("%d ", inter[i]);
+}
+return 0;
+}
